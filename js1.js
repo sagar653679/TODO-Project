835bce5f86aa44acc3e3401bf8a4e5eb95f4e59f
sagar653679 <sagar833@gmail.com>
2024-03-29 15:00:44 +0530

    first commit

---
+++ js1.js
@@ -0,0 +1,24 @@
+//document.getElementById("demo").innerHTML="hi this is sagar";
+
+/*const a=["a","b","c","d","e","f","g"];
+
+a.push("sagar","sowjanya");
+
+a[a.length]="s";
+a[1]="s"
+let flen=a.length;
+   
+
+let text="<ul>";
+
+for(let i=0;i<flen;i++)
+{
+
+text+="<li>"+a[i]+"</li>";
+
+
+}
+text+="</ul>";
+a.unshift("S");
+document.getElementById("demo").innerHTML=a.unshift("S");
+//document.getElementById("demo").innerHTML=a[1];*/
